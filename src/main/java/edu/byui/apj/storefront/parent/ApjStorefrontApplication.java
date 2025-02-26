86295298de8229e545b6556d8acd89e3d6d514b0
xuelwang7 <xueliwangcharlotte@foxmail.com>
2025-02-26 00:00:32 -0700

    Initial commit

---
+++ src/main/java/edu/byui/apj/storefront/parent/ApjStorefrontApplication.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApjStorefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApjStorefrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/byui/apj/storefront/parent/ApjStorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApjStorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
