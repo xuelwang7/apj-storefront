8a6b212acd55039b2604132afb03e53a2272952b
xuelwang7 <xueliwangcharlotte@foxmail.com>
2025-03-11 23:54:47 -0600

    week10-1

---
+++ tutorial10-1/src/test/java/apjstorefront/tutorial101/Tutorial101ApplicationTests.java
@@ -0,0 +1,13 @@
+package apjstorefront.tutorial101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
