80d60d5b0a3e5e6ba62e55ff98d1dc5e9ca32284
xuelwang7 <xueliwangcharlotte@foxmail.com>
2025-03-26 14:57:10 -0600

    w12-2

---
+++ tutorial12-2/src/main/java/apj/storefront/tutorial122/Tutorial122Application.java
@@ -0,0 +1,13 @@
+package apj.storefront.tutorial122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial122Application.class, args);
+	}
+
+}
---
+++ tutorial12-2/src/test/java/apj/storefront/tutorial122/Tutorial122ApplicationTests.java
@@ -0,0 +1,13 @@
+package apj.storefront.tutorial122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial122ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
