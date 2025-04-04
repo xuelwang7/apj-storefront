99410718b0dd7b4b979492a4fbbf7ba15ea4127c
xuelwang7 <xueliwangcharlotte@foxmail.com>
2025-04-03 22:20:39 -0600

    w8

---
+++ tutorial8-2/src/main/java/edu/byui/apj/storefront/parent/HelloController.java
@@ -0,0 +1,21 @@
+package edu.byui.apj.storefront.parent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
--- tutorial8-2/src/main/java/edu/byui/apj/storefront/parent/Tutorial82Application.java
+++ tutorial8-2/src/main/java/edu/byui/apj/storefront/parent/Tutorial82Application.java
@@ -1,4 +1,4 @@
-package apj.storefront;
+package edu.byui.apj.storefront.parent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
